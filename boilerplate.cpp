5f4351c367db1ac266823c00c6b76e956f1a11ae
Nilesh Pahari <nileshpahari@protonmail.com>
2025-04-12 18:00:02 +0530

    init commit

---
+++ boilerplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vip;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define nl '\n'
+#define pb push_back
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+void solve(){
+    
+}
+
+int main(){
+    fastio();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
